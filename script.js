42c4effdd4798826e1b9b98ec5f11f06679ee1a5
4JN22CS147 <gowdasharmadar@gmail.com>
2026-02-10 11:08:02 +0530

    fourth  commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeText() {
+    document.getElementById("output").innerHTML =
+        "Text changed using JavaScript!";
+}
+
+function changeImage() {
+    document.getElementById("mainImage").src = "image.png";
+    alert("Image button clicked!");
+}
